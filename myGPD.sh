91dc6579403d6dedc7c9ea2b736868375b7b1d8e
Lucas Pontes <105771594+P1xel4ce@users.noreply.github.com>
2022-06-28 22:42:44 -0300

    Add files via upload

---
+++ myGPD.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+##MyGpd##
+##PixelAce
+
+##Update System##
+sudo pacman -Syu --noconfirm
+sudo pacman -Syy --noconfirm
+
+##Uninstall Apps##
+echo '
+archcraft-help
+archcraft-about
+archcraft-openbox
+xarchiver
+gpick
+meld
+nitrogen
+plank
+atril
+baobab
+xfce4-terminal
+timeshift
+gparted' > removeApps.txt
+
+sudo pacman -Rns --noconfirm - < removeApps.txt
+
+##Install Apps#
+echo '
+archcraft-i3wm
+heroic-games-launcher-bin
+python
+python-pip
+mangohud
+lib32-mangohud
+sublime-text-4
+stremio
+youtube-music-bin
+cmatrix
+gotop
+gnome-disk-utility
+fish' > installApps.txt
+
+sudo pacman -S --noconfirm - < installApps.txt
+
+##.config##
+cp -r Dotfiles/i3/ /home/$USER/.config/			#Polybar/Wallpaper/Alacritty/Rofi
+cp -r Dotfiles/MangoHud/ /home/$USER/.config/	#Mangohud
+
+##.icons##
+cp -r icons/default/ /home/$USER/.icons/		#Tema do mouse
+
+##/etc##
+sudo cp -r etc/default/ /etc/					#Grub
+sudo cp -r etc/sddm.conf.d/ /etc/				#Tema do mouse do Sddm
+
+##SDDM##
+sudo cp -r usr/share/sddm/themes/archcraft/ /usr/share/sddm/themes/	#Wallpaper Sddm
+sudo cp -r usr/share/sddm/scripts/ /usr/share/sddm/		#Configura Sddm
+
+##Apply grub config##
+sudo update-grub
+
+##Reboot system##
+reboot
