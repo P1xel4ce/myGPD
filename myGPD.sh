f5f1339f57c831a7247c75476298a8c394587c76
Lucas Pontes <105771594+P1xel4ce@users.noreply.github.com>
2022-06-28 23:05:39 -0300

    Add files via upload

--- myGPD.sh
+++ myGPD.sh
@@ -44,8 +44,8 @@ fish' > installApps.txt
 sudo pacman -S --noconfirm - < installApps.txt
 
 ##.config##
-cp -r Dotfiles/i3/ /home/$USER/.config/			#Polybar/Wallpaper/Alacritty/Rofi
-cp -r Dotfiles/MangoHud/ /home/$USER/.config/	#Mangohud
+cp -r config/i3/ /home/$USER/.config/			#Polybar/Wallpaper/Alacritty/Rofi
+cp -r config/MangoHud/ /home/$USER/.config/	#Mangohud
 
 ##.icons##
 cp -r icons/default/ /home/$USER/.icons/		#Tema do mouse
